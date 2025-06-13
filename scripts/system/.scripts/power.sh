86b6fe332e7fe84aa024f69bb067ba4374242a56
khedrinhoo <khedrinhoo@gmail.com>
2025-06-13 12:59:20 +0000

    power script update

--- scripts/system/.scripts/power.sh
+++ scripts/system/.scripts/power.sh
@@ -2,7 +2,7 @@
 
 choice=$(printf "Lock\nReboot\nExit\nShutdown" | dmenu -p "Choose action: ")
 
-[ $choice = "Lock"    ] && sudo suspend
-[ $choice = "Reboot"  ] && sudo reboot
+[ $choice = "Lock"    ] && systemctl suspend
+[ $choice = "Reboot"  ] && systemctl reboot
 [ $choice = "Exit"    ] && sudo pkill dwm
-[ $choice = "Shutdown"] && sudo poweroff
+[ $choice = "Shutdown"] && systemctl poweroff
