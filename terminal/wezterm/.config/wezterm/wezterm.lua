42f8a3ba54ea5b0afdcf3417bb0d4ef17e4c7835
khedrinhoo <khedrinhoo@gmail.com>
2025-06-23 20:15:54 +0300

    wezterm configuration

--- terminal/wezterm/.config/wezterm/wezterm.lua
+++ terminal/wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- colors
@@ -8,7 +8,8 @@ config.colors = {
 }
 
 -- appearance
-config.hide_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+
 
 -- settings
 config.max_fps = 120
