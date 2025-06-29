35ae8571d14e6529ef3e6437b43bf3394b2fb3dc
khedrinhoo <khedrinhoo@gmail.com>
2025-06-29 15:17:53 +0300

    A minimal guide

--- terminal/wezterm/.config/wezterm/wezterm.lua
+++ terminal/wezterm/.config/wezterm/wezterm.lua
@@ -1,15 +1,11 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- colors
-config.colors = {
-	cursor_bg = "white",
-	cursor_border = "white",
-}
+-- color_scheme
+config.color_scheme = 'Oxocarbon Dark'
 
 -- appearance
 config.hide_tab_bar_if_only_one_tab = true
-
 
 -- settings
 config.max_fps = 120
