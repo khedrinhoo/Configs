d8b88546d13c6ee52878915a37c284b066ef4cb9
khedrinhoo <khedrinhoo@gmail.com>
2026-01-27 20:51:39 +0200

    New Updates

--- install.sh
+++ install.sh
@@ -2,26 +2,22 @@
 
 # Dev
 sudo pacman -S --noconfirm --needed git base-devel stow openssh
-# AUR
-cd $HOME/.src
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si --noconfirm
-cd $HOME/configs 
 # Hardware
 sudo pacman -S --noconfirm --needed bluez bluez-utils networkmanager 
-sudo pacman -S --noconfirm --needed pipewire pipewire-audio pipewire-pulse pipewire-alsa pipewire-jack alsa-utils
+sudo pacman -S --noconfirm --needed pipewire pipewire-audio pipewire-pulse pipewire-alsa alsa-utils
 # Fonts
 sudo pacman -S --noconfirm --needed noto-fonts noto-fonts-emoji 
 # Window Manager
-sudo pacman -S --noconfirm --needed hyprland hyprshot waybar bemenu-wayland 
+sudo pacman -S --noconfirm --needed hyprland hyprshot waybar bemenu-wayland sway swaybg i3blocks
 # Web
 sudo pacman -S --noconfirm --needed firefox chromium
 # Terminal
 sudo pacman -S --noconfirm --needed kitty tmux gvim nnn htop fzf eza
 # Media
-sudo pacman -S --noconfirm --needed sxiv zathura mpv mpd libreoffice unzip
+sudo pacman -S --noconfirm --needed sxiv zathura mpv mpd unzip 
+# KDE Programs
+sudo pacman -S --noconfirm --needed dolphin okular gwenview spectacle kdenlive 
 # Systemd
 sudo systemctl enable --now bluetooth
 sudo systemctl enable --now NetworkManager
-sudo systemctl enable --now pipewire pipewire-pulse
+systemctl --user enable --now pipewire pipewire-pulse
