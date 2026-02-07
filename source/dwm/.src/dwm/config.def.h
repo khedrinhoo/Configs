4eea32bc33d330e79f7cc055781b417983375584
euler <amr.euler@gmail.com>
2026-02-07 16:34:03 +0200

    DWM setup

--- source/dwm/.src/dwm/config.def.h
+++ source/dwm/.src/dwm/config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminus :size=10" };
-static const char dmenufont[]       = "Terminus :size=10";
+static const char *fonts[]          = { "JetBrains Mono :size=10" };
+static const char dmenufont[]       = "JetBrains Mono :size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -59,12 +59,12 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 
-	{ MODKEY,             		      XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,             		    XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 
 	{ MODKEY,                       XK_h,      focusdir,       {.i = 0 } },
