20c0fcf0cbf85d0e5a1dde795df2b36574c1ac71
euler <amr.euler@gmail.com>
2026-02-07 16:22:42 +0200

    DWM setup

--- install.sh
+++ install.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
 # Dev
-sudo pacman -S --noconfirm --needed git base-devel stow openssh
-# Hardware
-sudo pacman -S --noconfirm --needed bluez bluez-utils networkmanager 
+sudo pacman -S --noconfirm --needed git wget curl base-devel openssh stow
+
+# Suckless
+sudo pacman -S --noconfirm --needed libx11 libxft libxinerama xorg-server xorg-xinit
+
+# Network
+sudo pacman -S --noconfirm --needed networkmanager 
+# Bluetooth
+sudo pacman -S --noconfirm --needed bluez bluez-utils 
+# Audio
 sudo pacman -S --noconfirm --needed pipewire pipewire-audio pipewire-pulse pipewire-alsa alsa-utils
+
 # Fonts
-sudo pacman -S --noconfirm --needed noto-fonts noto-fonts-emoji 
-# Window Manager
-sudo pacman -S --noconfirm --needed bemenu-wayland sway swaybg i3blocks
+sudo pacman -S --noconfirm --needed noto-fonts noto-fonts-emoji ttf-jetbrains-mono
+
 # Web
 sudo pacman -S --noconfirm --needed firefox chromium
+
 # Terminal
-sudo pacman -S --noconfirm --needed kitty tmux gvim nnn htop eza
+sudo pacman -S --noconfirm --needed tmux vim nnn btop htop fzf
+
 # Media
 sudo pacman -S --noconfirm --needed sxiv zathura mpv mpd unzip 
+
 # Systemd
 sudo systemctl enable --now bluetooth
 sudo systemctl enable --now NetworkManager
